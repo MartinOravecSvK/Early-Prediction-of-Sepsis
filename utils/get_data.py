59bd640df6f3542cf2e00fc22651313fa1ad4315
MartinOravecSvK <oravec.martin01@gmail.com>
2024-03-14 09:43:35 +0000

    Started work on making hypertools ppca work

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 
 # Returns the absolute path of the dataset directory.
@@ -8,3 +9,22 @@ def get_dataset_abspath():
     DATA_PATH = parent_dir + '/Dataset/'
 
     return DATA_PATH
+
+def get_dataset_as_df():
+    data = []
+    c = 0
+    DATA_PATH = get_dataset_abspath()
+    file_listA = os.listdir(DATA_PATH + 'training_setA/')
+    file_listB = os.listdir(DATA_PATH + 'training_setB/')
+
+    for file in file_listA:
+        data.append(pd.read_csv(DATA_PATH + 'training_setA/' + file))
+        print(c, end='\r')
+        c += 1
+
+    for file in file_listB:
+        data.append(pd.read_csv(DATA_PATH + 'training_setB/' + file))
+        print(c, end='\r')
+        c += 1
+    
+    return data
