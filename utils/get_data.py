8737ff2cfdd20b216f4622821385060357baa0d6
LewisClifton <lewisclifton2003@gmail.com>
2024-03-17 22:20:00 +0000

    updated getting dataset utility

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import hypertools as hyp
+# import hypertools as hyp
 import os
 import matplotlib.pyplot as plt
-from ppca import PPCA
+# from ppca import PPCA
+from tqdm import tqdm
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -17,63 +18,36 @@ def get_dataset_abspath():
 
     return DATA_PATH
 
-def get_dataset_as_df():
-    data = []
-    c = 0
-    DATA_PATH = get_dataset_abspath()
-    file_listA = os.listdir(DATA_PATH + 'training_setA/')
-    file_listB = os.listdir(DATA_PATH + 'training_setB/')
+def get_dataset_as_df(files=-1):
 
-    for file in file_listA:
-        data.append(pd.read_csv(DATA_PATH + 'training_setA/' + file))
-        print("  ", c, end='\r')
-        c += 1
-    print("  ", c)
-    for file in file_listB:
-        data.append(pd.read_csv(DATA_PATH + 'training_setB/' + file))
-        print("  ", c, end='\r')
-        c += 1
-    print("  ", c)
-    
-    print("Putting data into dataframe...")
-    # dataset = pd.concat(data)
-    dataset = pd.concat(data)
-    print("Done")
+    return pd.DataFrame( get_dataset_as_np(files) )
 
-    return dataset
-
-def get_dataset_as_np(include_strings=False):
-    data = []
-    c = 0
+def get_dataset_as_np(files=-1):
+  
     DATA_PATH = get_dataset_abspath()  # Ensure this function is defined and returns the correct path
-    file_listA = os.listdir(DATA_PATH + 'training_setA/')
-    file_listB = os.listdir(DATA_PATH + 'training_setB/')
+    file_listA = list(map(lambda file: DATA_PATH + 'training_setA/' + file, os.listdir(DATA_PATH + 'training_setA/'))) 
+    file_listB = list(map(lambda file: DATA_PATH + 'training_setB/' + file, os.listdir(DATA_PATH + 'training_setB/'))) 
+    file_list = file_listA + file_listB
+    total_files = len(file_list)
 
-    for file in file_listA:
-        df = pd.read_csv(DATA_PATH + 'training_setA/' + file, na_values=['NA', 'na', ''])
-        data.append(df)
-        print("  ", c, end='\r')
-        c += 1
+    if files == -1:
+        files = total_files
 
-    for file in file_listB:
-        df = pd.read_csv(DATA_PATH + 'training_setB/' + file, na_values=['NA', 'na', ''])
-        data.append(df)
-        print("  ", c, end='\r')
-        c += 1
-    print("  ", c)
+    files = min(files, total_files)
 
-    print("Combining data into a single DataFrame and converting to numpy array...")
-    # print(data.shape)
-    combined_df = pd.concat(data)
-    print(combined_df.shape)
-    # Ensure the data is numeric and convert to float
-    numeric_df = combined_df.apply(pd.to_numeric, errors='coerce')
-    dataset = numeric_df.astype(float).to_numpy()
+    dataset = np.array([])
 
-    if not include_strings:
-        dataset = preprocess_no_strings(combined_df)
+    print("Loading dataset...")
+    with tqdm(total=files) as progress_bar:
+        for file in file_list[:files]:
+            file_data = np.genfromtxt(file, delimiter='|').astype(float)
+            if dataset.size == 0:
+                dataset = file_data
+            else:
+                dataset = np.vstack([dataset, file_data])
+            progress_bar.update(1)
 
-    print("Done")
+    print("Done.")
     return dataset
 
 def preprocess_no_strings(df):
