90a63eab3b9acc57fc355330855fe6a614043204
MartinOravecSvK <oravec.martin01@gmail.com>
2024-05-01 11:01:26 +0100

    Added one models trained wheights and very minor cahnges

--- transformer_test_run.py
+++ transformer_test_run.py
@@ -49,41 +49,6 @@ from torch.nn.functional import pad
 
 # ___________________________________________________________________________________________________________________________________
 
-# class PositionalEncoding(nn.Module):
-#     def __init__(self, d_model, dropout=0.1, max_len=5000):
-#         super(PositionalEncoding, self).__init__()
-#         self.dropout = nn.Dropout(p=dropout)
-
-#         pe = torch.zeros(max_len, d_model)
-#         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-#         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model))
-#         pe[:, 0::2] = torch.sin(position * div_term)
-#         pe[:, 1::2] = torch.cos(position * div_term)
-#         pe = pe.unsqueeze(0).transpose(0, 1)
-#         self.register_buffer('pe', pe)
-
-#     def forward(self, x):
-#         x = x + self.pe[:x.size(0), :]
-#         return self.dropout(x)
-
-
-# class TransformerTimeSeries(nn.Module):
-#     def __init__(self, input_dim=1, d_model=64, nhead=4, num_layers=2, dropout=0.2):
-#         super(TransformerTimeSeries, self).__init__()
-
-#         self.encoder = nn.Linear(input_dim, d_model)
-#         self.pos_encoder = PositionalEncoding(d_model, dropout)
-#         encoder_layers = nn.TransformerEncoderLayer(d_model, nhead)
-#         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
-#         self.decoder = nn.Linear(d_model, 1)
-
-#     def forward(self, x):
-#         x = self.encoder(x)
-#         x = self.pos_encoder(x)
-#         x = self.transformer_encoder(x.transpose(0, 1))
-#         x = self.decoder(x)
-#         return x.squeeze(-1)
-
 class PatientDataset(Dataset):
     def __init__(self, patient_ids, X, y, max_length, device):
         self.patient_ids = patient_ids
@@ -301,7 +266,7 @@ def main():
         num_epochs = 10
         generator = torch.Generator(device="cpu")
         batch_size = 128
-        num_workers = 4
+        num_workers = 12
         dataloader = DataLoader(PatientDataset(train_ids, X, y, max_length, device), 
                                 batch_size=batch_size, 
                                 shuffle=True, 
